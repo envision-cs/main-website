5f89d2feb1023ae9d3844af6d84c44de5b037ecb
clpowe <clpowe@gmail.com>
2025-12-01 21:41:18 -0500

    Worked on about page created sticky component

--- app/components/app/banner-b.vue
+++ app/components/app/banner-b.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineProps<{
-  image: string;
+  image?: string;
 }>();
 </script>
 
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -24,42 +24,58 @@ const core = [
 </script>
 
 <template>
-  <UPage class="mt-0 ">
-    <div class="grid">
-      <app-hero-banner title="About Us" />
+  <div>
+    <app-banner-b>
+      <template #title>
+        Envision
+      </template>
+      About Us
+    </app-banner-b>
+    <app-section-a flip>
+      <template #sticky>
+        <app-typography
+          variant="heading-lg"
+          tag="h2"
+          class=""
+        >
+          Our <span>History</span>
+        </app-typography>
+      </template>
+      <template #body>
+        <app-typography variant="text-md" tag="p">
+          Allen is the Managing Director on the Leadership Team for Envision, an MBE-certified construction management firm.
+        </app-typography>
 
-      <app-banner
-        flip
-        title="Our Orign Story"
-        description="Founded in 2009 by Allen Greene Sr. and Allen Greene II, Envision was born from a simple conviction: build a construction firm where people come first—every time. Based in Tampa and family-owned, we’ve grown from humble beginnings into a trusted partner across Florida by holding fast to our faith, serving others, and refusing to settle for 'good enough.'"
-      />
-      <app-banner
-        title="Mission & Purpose"
-        description="Built on Faith. Driven by Purpose. Focused on Legacy.
-We don’t just build structures—we elevate communities, mentor small businesses, and shape spaces that stand the test of time. Our mission is to build people while building projects.
-"
-      />
-      <three-uniques />
+        <app-typography variant="text-md" tag="p">
+          In 2009, Allen and his son established Envision on the principles of integrity, building people, and relationships. This came just a year following the economic downturn of 2008, when Allen and his son suffered extreme financial hardship and lost all of their contracts, employees, and practically everything they owned. A year later, with a renewed mindset built on faith, the father-son duo set out to build a company bigger than themselves. Over a decade later, the two have created a company where an “It’s Bigger Than You” philosophy has shaped the company’s culture both internally and externally—a culture that can be recognized on every project and throughout their team.
+        </app-typography>
 
-      <app-banner
-        title="Our Core Values"
-        description="Choosing the wrong partner can mean wasted budget, endless delays, and a space that never meets its potential. We protect your investment, your timeline, and your vision"
-      />
-      <div>
-        <ul class="site-grid">
-          <li
-            v-for="item in core"
-            :key="item.title"
-            class="item"
-          >
-            <h3 class="mt-2 text-xl text-balance leading-5 sm:text-2xl sm:leading-8 font-semibold">
-              {{ item.title }}
-            </h3>
-          </li>
-        </ul>
-      </div>
+        <app-typography variant="text-md" tag="p">
+          With over 30 years of experience in the construction industry, Allen has led project teams and worked across numerous building types, such as higher education, hospitals, hospitality, worship centers, and more.
+        </app-typography>
+      </template>
+    </app-section-a>
+
+    <three-uniques />
+
+    <app-banner
+      title="Our Core Values"
+      description="Choosing the wrong partner can mean wasted budget, endless delays, and a space that never meets its potential. We protect your investment, your timeline, and your vision"
+    />
+    <div>
+      <ul class="site-grid">
+        <li
+          v-for="item in core"
+          :key="item.title"
+          class="item"
+        >
+          <h3 class="mt-2 text-xl text-balance leading-5 sm:text-2xl sm:leading-8 font-semibold">
+            {{ item.title }}
+          </h3>
+        </li>
+      </ul>
     </div>
-  </UPage>
+  </div>
 </template>
 
 <style scoped>
