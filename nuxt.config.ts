fc2ea39a6a7b55fd8f1753a5950d621f86048772
clpowe <clpowe@gmail.com>
2026-01-03 22:38:58 -0500

    updated the favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
