8d035c3747a863d6b123551b58168ae288b4876e
clpowe <clpowe@gmail.com>
2026-03-12 18:01:49 -0400

    small change

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -4,9 +4,7 @@
 
 <template>
   <div>
-    <app-banner-b
-      image="about/about03.jpg"
-    >
+    <app-banner-b image="about/about03.jpg">
       About Us
     </app-banner-b>
     <app-section-a flip>
@@ -16,7 +14,7 @@
           tag="h2"
           class=""
         >
-          Our <span>History</span>
+          Our<span> History</span>
         </app-typography>
       </template>
       <template #body>
@@ -25,7 +23,8 @@
           tag="p"
           class="max-w-[60ch] text-pretty"
         >
-          Allen is the Managing Director on the Leadership Team for Envision, an MBE-certified construction management firm.
+          Allen is the Managing Director on the Leadership Team for Envision, an MBE-certified construction management
+          firm.
         </app-typography>
 
         <app-typography
@@ -33,7 +32,13 @@
           tag="p"
           class="max-w-[60ch] text-pretty"
         >
-          In 2009, Allen and his son established Envision on the principles of integrity, building people, and relationships. This came just a year following the economic downturn of 2008, when Allen and his son suffered extreme financial hardship and lost all of their contracts, employees, and practically everything they owned. A year later, with a renewed mindset built on faith, the father-son duo set out to build a company bigger than themselves. Over a decade later, the two have created a company where an “It’s Bigger Than You” philosophy has shaped the company’s culture both internally and externally—a culture that can be recognized on every project and throughout their team.
+          In 2009, Allen and his son established Envision on the principles of integrity, building people, and
+          relationships. This came just a year following the economic downturn of 2008, when Allen and his son suffered
+          extreme financial hardship and lost all of their contracts, employees, and practically everything they owned.
+          A year later, with a renewed mindset built on faith, the father-son duo set out to build a company bigger than
+          themselves. Over a decade later, the two have created a company where an “It’s Bigger Than You” philosophy has
+          shaped the company’s culture both internally and externally—a culture that can be recognized on every project
+          and throughout their team.
         </app-typography>
 
         <app-typography
@@ -41,7 +46,8 @@
           tag="p"
           class="max-w-[60ch] text-pretty"
         >
-          With over 30 years of experience in the construction industry, Allen has led project teams and worked across numerous building types, such as higher education, hospitals, hospitality, worship centers, and more.
+          With over 30 years of experience in the construction industry, Allen has led project teams and worked across
+          numerous building types, such as higher education, hospitals, hospitality, worship centers, and more.
         </app-typography>
       </template>
     </app-section-a>
