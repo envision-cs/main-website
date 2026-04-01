9ae047138cb269ee666aea68a0f2115fa97b1253
clpowe <clpowe@gmail.com>
2026-03-31 23:31:03 -0400

    added proven process

--- app/components/proven-process.vue
+++ app/components/proven-process.vue
@@ -40,77 +40,86 @@ const process = [
 <template>
   <section>
     <div class="content">
-      <div class="grid gap-4">
-        <app-typography tag="h2" variant="heading-md" class="font-semibold">
-          Our Proven Process
-        </app-typography>
+      <app-typography tag="h2" variant="heading-lg" bold="true">
+        Our Proven Process
+      </app-typography>
 
-        <app-typography tag="h2">
-          Our proven process brings structure and consistency to every project. We listen, plan, and
-          execute to reduce risk, maintain alignment, and deliver on time, on budget, and with
-          complete transparency.
-        </app-typography>
-      </div>
-
-      <ul class="mt-auto">
-        <li v-for="item in process" :key="item.id" class="flex flex-col gap-4">
-          <div class="grid gap-2">
-            <app-typography tag="h3" variant="heading-lg" class="number font-semibold">
-              {{ item.order }}
-            </app-typography>
-            <app-typography tag="h3" variant="heading-lg" class="font-semibold">
-              {{ item.title }}
-            </app-typography>
-          </div>
-          <app-typography tag="p">
-            {{ item.description }}
-          </app-typography>
-        </li>
-      </ul>
+      <app-typography tag="h2">
+        Our proven process brings structure and consistency to every project. We listen, plan, and
+        execute to reduce risk, maintain alignment, and deliver on time, on budget, and with
+        complete transparency.
+      </app-typography>
     </div>
+
+    <ul>
+      <li v-for="item in process" :key="item.id" class="flex flex-col gap-4">
+        <div class="grid gap-2">
+          <app-typography tag="h3" variant="heading-lg" class="number font-semibold">
+            {{ item.order }}
+          </app-typography>
+          <app-typography tag="h3" variant="heading-lg" class="font-semibold">
+            {{ item.title }}
+          </app-typography>
+        </div>
+        <app-typography tag="p">
+          {{ item.description }}
+        </app-typography>
+      </li>
+    </ul>
   </section>
 </template>
 
 <style scoped>
 section {
+  display: grid;
   grid-column: 1/-1;
-  padding: calc(var(--spacing) * 4);
-  container-type: inline-size;
-  container-name: section;
-
-  @media (min-width: 768px) {
-    padding-inline: calc(var(--spacing) * 8);
-    padding-block: calc(var(--spacing) * 20);
-  }
+  grid-template-columns: subgrid;
+  gap: calc(var(--spacing) * 8);
+  padding-inline: calc(var(--spacing) * 4);
+  padding-block: calc(var(--spacing) * 16);
 }
 
 .content {
-  display: grid;
-  justify-content: stretch;
-  gap: calc(var(--spacing) * 16);
-  margin-inline: auto;
-
-  @media (min-width: 768px) {
-    justify-content: space-between;
-  }
+  grid-column: 1/-1;
+  display: flex;
+  flex-direction: column;
+  gap: calc(var(--spacing) * 2);
 }
 
 ul {
   display: grid;
-  grid-template-columns: 1fr;
+  grid-column: 1/-1;
+  grid-template-columns: subgrid;
+
   gap: calc(var(--spacing) * 8);
-  counter-reset: step;
-
-  @container section (width > 600px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  @container section (width > 1200px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
 }
 
 li {
-  position: relative;
+  padding: calc(var(--spacing) * 3);
+  grid-column: span 2;
+
+  @media (min-width: 700px) {
+    grid-column: span 6;
+  }
+
+  @media (min-width: 1024px) {
+    grid-column: span 6;
+  }
+}
+
+li:nth-child(1) {
+  background-color: var(--color-envision-blue-600);
+}
+
+li:nth-child(2) {
+  background-color: var(--color-envision-green-500);
+}
+
+li:nth-child(3) {
+  background-color: var(--color-envision-gray-700);
+}
+
+li:nth-child(4) {
+  background-color: #435a66;
 }
 </style>
